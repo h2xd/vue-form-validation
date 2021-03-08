491d748c3cb1c3cc5668ebfd7f69a27ef19e8d03
Andreas Schujkow <andreas.schujkow@sumcumo.com>
2021-03-09 00:31:25 +0100

    feat(vite): add build path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+
+
+export default ({ command, mode }) => {
+  if (command === 'serve') {
+    return defineConfig({
+      plugins: [vue()]
+    })
+  } else {
+    return defineConfig({
+      base: 'https://h2xd.github.io/vue-form-validation/',
+      plugins: [vue()]
+    })
+  }
+}
